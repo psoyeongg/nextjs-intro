cc78203156e4817dca100db0b29caa5b692d91ea
sypark96 <sypark96@nkia.co.kr>
2022-03-14 13:54:38 +0900

    SSR 적용

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
-import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
-
-export default function Home() {
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    // async 부분이 익명 함수(재사용 불가)로 작성되었고, 익명 함수는 즉시 실행해야하기 때문에 ()를 이용해 익명 함수를 바로 호출
-    (async () => {
-      const { results } = await ( 
-        await fetch('/api/movies')
-        ).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div className='container'>
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies && movies.map((movie) => (
+      {results.map((movie) => (
         <div className='movie' key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
           <h4>{movie.original_title}</h4>
@@ -48,4 +33,13 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await ( await fetch('http://localhost:3000/api/movies')).json();
+  return  {
+    props: {
+      results,
+    }
+  }
 }
