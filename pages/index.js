5bf11d67b398e63fd663633370f04b706b9898a6
sypark96 <sypark96@nkia.co.kr>
2022-03-14 11:02:38 +0900

    인기영화 api 연결 및 스타일 추가

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import Seo from "../components/Seo";
+
+const  API_KEY = "f0d901061752dd9758fcd068aa9b8c3a"
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await ( 
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+          )
+        ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies && movies.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
